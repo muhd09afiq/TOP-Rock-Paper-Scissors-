f1adbaf31fba7e4391def8b3868d66027a689221
blazion09 <muhd09afiq@gmail.com>
2023-11-20 14:29:34 +0800

    chore wewrite with IF function
    rework the logic to keep count

--- script.js
+++ script.js
@@ -8,14 +8,26 @@
 
 let randomNumber = () => Math.round((Math.random()) * 10);
 
-let outputResult = () => {
+// let outputResult = () => {
+//     if (randomNumber() <= 3){
+//        outputResult = 'rock';
+//     } else if (randomNumber() <= 6 && randomNumber() > 3) {
+//     outputResult = 'paper';
+//     } else {
+//         outputResult = 'scissor';    
+//     }
+// }
+
+let cpuChoice;
+
+function outputResult(){
     if (randomNumber() <= 3){
-       outputResult = 'rock';
-    } else if (randomNumber() <= 6 && randomNumber() > 3) {
-    outputResult = 'paper';
-    } else {
-        outputResult = 'scissor';    
-    }
+        cpuChoice = 'rock';
+     } else if (randomNumber() <= 6 && randomNumber() > 3) {
+        cpuChoice = 'paper';
+     } else {
+        cpuChoice = 'scissor';    
+     }
 }
 
 // Write a function that plays a single round of Rock Paper Scissors
@@ -38,40 +50,41 @@ let outputResult = () => {
 //            IF computerselection paper PRINT You win
 //            IF computerselection scissor PRINT Draw
 
-let playerinput = (prompt('Choose rock, paper or scissor')).toLowerCase();
+let playerinput;
 console.log('Your choice is: ' + playerinput);
-playRound();
 
 function playRound(){
+playerinput = (prompt('Choose rock, paper or scissor')).toLowerCase();
 
 if (playerinput === 'rock'){  
     outputResult();
-    console.log('CPU choice is: ' + outputResult);
-    if (outputResult === "rock"){
+    console.log('CPU choice is: ' + cpuChoice);
+    if (cpuChoice === "rock"){
         console.log("Draw");
-    } else if (outputResult === "paper"){
+    } else if (cpuChoice === "paper"){
         console.log("You lose");
-    } else if (outputResult === "scissor"){
+    } else if (cpuChoice === "scissor"){
         console.log("You win");
     }
 } else if (playerinput === 'paper'){
     outputResult();
-    console.log('CPU choice is: ' + outputResult);
-    if (outputResult === "rock"){
+    console.log('CPU choice is: ' + cpuChoice);
+    if (cpuChoice === "rock"){
         console.log("You win");
-    } else if (outputResult === "paper"){
+    } else if (cpuChoice === "paper"){
         console.log("Draw");
-    } else if (outputResult === "scissor"){
+    } else if (cpuChoice === "scissor"){
         console.log("You lose");
     }
 } else if (playerinput === 'scissor'){
     outputResult();
-    console.log('CPU choice is: ' + outputResult);
-    if (outputResult === "rock"){
+    console.log('CPU choice is: ' + cpuChoice);
+    if (cpuChoice === "rock"){
         console.log("You lose");
-    } else if (outputResult === "paper"){
+    } else if (cpuChoice === "paper"){
         console.log("You win");
-    } else if (outputResult === "scissor"){
+        
+    } else if (cpuChoice === "scissor"){
         console.log("Draw");
     }
 } else {
@@ -79,3 +92,55 @@ if (playerinput === 'rock'){
 }
 }
 
+let scorePlayer = 0;
+let scoreCPU = 0;
+
+function score() {
+    if (console.log(playRound) === 'You win'){
+        console.log(1);
+    } else {
+        console.log(2);
+    }
+}
+
+function game(){
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+}
+
+
+// score();
+
+// function score(){
+//     if playRound
+
+// }
+
+
+// NEW function called game()
+// to play a 5 round game that keeps score and reports a winner or loser at the end.
+// using console.log() to display the results of each round and the winner at the end.
+// 1. play 5 round of playRound
+// 2. keep score of each result of playRound
+// 3. calculate the winner and show the result
+//  FOR playRound
+//      INIT score for player
+//      INIT score for cpu
+//          ADD 1 for each win
+//      
+//      PRINT winner
+//      GET winner
+//  REPEAT 5 TIMES
+//
+
+function game(){
+    playRound();
+    let score
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+}
