417325e6caef838ab0f04558bf4c124a25bb9b06
blazion09 <muhd09afiq@gmail.com>
2023-12-12 14:09:56 +0800

    remove comment and disable game

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// begin with a function called getComputerChoice that will
-// randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
-// 
-// GET random number (JScript Math.random() return random number below 1)
-//  FROM random number (range of 0 to 0.9999999 > lets turn it to integer 1-10)
-//    DETERMINE rock, paper, scissor (rock = 1 to 3 / paper = 4 to 6 / scissor = 7 to 9)
-//    PRINT rock/paper/scissor
-
 let randomNumber = () => Math.round((Math.random()) * 10);
 let cpuChoice;
 
@@ -18,26 +10,6 @@ function outputResult(){
         cpuChoice = 'scissor';    
      }
 }
-
-// Write a function that plays a single round of Rock Paper Scissors
-// The function should take two parameters - the playerSelection and computerSelection - 
-// and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-// 
-// GET playerinput (rock/paper/scissor)
-// GET computerselection (outputResult() = rock/paper/scissor)
-//     CASE playerinput
-//          OF rock
-//            IF computerselection rock PRINT Draw
-//            IF computerselection paper PRINT You lose
-//            IF computerselection scissor PRINT You win
-//          OF paper
-//            IF computerselection rock PRINT You win
-//            IF computerselection paper PRINT Draw
-//            IF computerselection scissor PRINT You lose
-//          OF scissor
-//            IF computerselection rock PRINT You lose
-//            IF computerselection paper PRINT You win
-//            IF computerselection scissor PRINT Draw
 
 let playerinput;
 
@@ -95,24 +67,8 @@ function score(){
         console.log('You lose the game with a score of ' + playerScore + ' out of 5')
     }   
 }
-game();
-score();
-
-// NEW function called game()
-// to play a 5 round game that keeps score and reports a winner or loser at the end.
-// using console.log() to display the results of each round and the winner at the end.
-// 1. play 5 round of playRound
-// 2. keep score of each result of playRound
-// 3. calculate the winner and show the result
-//  FOR playRound
-//      INIT score for player
-//      INIT score for cpu
-//          ADD 1 for each win
-//      
-//      PRINT winner
-//      GET winner
-//  REPEAT 5 TIMES
-//
+//game();
+//score();
 
 function game(){
     playRound();
